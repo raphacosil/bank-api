bfb5e0b86803ebb8831ecc8d6589c99910569ed3
raphacosil <raphaelcos.worker@gmail.com>
2026-03-05 10:30:51 -0300

    Adding balance logic

--- src/main/java/com/example/bank_api/repository/BalanceRepository.java
+++ src/main/java/com/example/bank_api/repository/BalanceRepository.java
@@ -3,8 +3,10 @@ package com.example.bank_api.repository;
 import com.example.bank_api.model.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BalanceRepository extends JpaRepository<Balance, Long> {
-     Balance findByCustomerId(Long customerId);
+import java.util.Optional;
 
-     Double findAmountByCustomerId(Long customerId);
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
+     Optional<Balance> findByCustomerId(Long customerId);
+
+     Optional<Double> findAmountByCustomerId(Long customerId);
 }
---
+++ src/main/java/com/example/bank_api/service/BalanceService.java
@@ -0,0 +1,31 @@
+package com.example.bank_api.service;
+
+import com.example.bank_api.exception.NotFoundException;
+import com.example.bank_api.model.Balance;
+import com.example.bank_api.repository.BalanceRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class BalanceService {
+    BalanceRepository balanceRepository;
+
+    public Balance getBalanceByCustomer(Long customerId){
+        Optional<Balance> balance = balanceRepository.findByCustomerId(customerId);
+        if(balance.isEmpty()){
+            throw new NotFoundException();
+        }
+        return balance.get();
+    }
+
+    public Double getBalanceAmountByCustomer(Long customerId){
+        Optional<Double> amount = balanceRepository.findAmountByCustomerId(customerId);
+        if(amount.isEmpty()){
+            throw new NotFoundException();
+        }
+        return amount.get();
+    }
+}
