3329246e6d214984d3ccd1553fe37d89f41913d7
raphacosil <raphaelcos.worker@gmail.com>
2026-03-12 16:39:49 -0300

    Adding transfer and refund tests

--- src/test/java/com/example/bank_api/service/TransferenceServiceTest.java
+++ src/test/java/com/example/bank_api/service/TransferenceServiceTest.java
@@ -3,6 +3,7 @@ package com.example.bank_api.service;
 import com.example.bank_api.exception.NotFoundException;
 import com.example.bank_api.exception.UnprocessableEntityException;
 import com.example.bank_api.gateway.ApiGateway;
+import com.example.bank_api.model.Balance;
 import com.example.bank_api.model.Transference;
 import com.example.bank_api.repository.BalanceRepository;
 import com.example.bank_api.repository.CustomerRepository;
@@ -16,7 +17,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,27 +36,145 @@ public class TransferenceServiceTest {
     @InjectMocks
     TransferenceService transferenceService;
 
-//    public List<Transference> findAll(){
-//        return transferenceRepository.findAll();
-//    }
-//    public Transference findById(Long transferenceId){
-//        Optional<Transference> transference = transferenceRepository.findById(transferenceId);
-//
-//        if(transference.isEmpty()){
-//            throw new NotFoundException();
-//        }
-//
-//        return transference.get();
-//    }
-//
-//    public List<Transference> findByCustomer(Long customerId){
-//        return transferenceRepository.findByCustomer(customerId);
-//    }
-//
-//    public List<Transference> findBetweenCustomers(Long firstCustomerId, Long secondCustomerId){
-//        return transferenceRepository.findByCustomer(firstCustomerId, secondCustomerId);
-//    }
+    @Test
+    void whenTransferAndPayerEqualPayee_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(1L, 1L, 1L, 100.00, null);
 
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.transfer(transference));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+        assertEquals("Unprocessable entity Payer and payee should be different", exception.getMessage());
+    }
+
+    @Test
+    void whenTransferAndPayerIsBusiness_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+        when(customerRepository.findIsBusinessById(1L)).thenReturn(true);
+
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.transfer(transference));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+        assertEquals("Unprocessable entity Payer should not be a business", exception.getMessage());
+    }
+
+    @Test
+    void whenTransferAndPayerBalanceIsNotFound_thenThrowNotFoundException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+
+        when(customerRepository.findIsBusinessById(1L)).thenReturn(false);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> transferenceService.transfer(transference));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+    }
+
+    @Test
+    void whenTransferAndPayeeBalanceIsNotFound_thenThrowNotFoundException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+        Balance balance = new Balance(1L, 1L, 100.00);
+        Optional<Balance> balanceOptional = Optional.of(balance);
+
+        when(customerRepository.findIsBusinessById(1L)).thenReturn(false);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(balanceOptional);
+        when(balanceRepository.findByCustomerId(2L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> transferenceService.transfer(transference));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+    }
+
+    @Test
+    void whenTransferAndNotEnoughBalance_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(1L, 1L, 2L, 150.00, null);
+        Balance balance = new Balance(1L, 1L, 100.00);
+        Optional<Balance> balanceOptional = Optional.of(balance);
+
+        when(customerRepository.findIsBusinessById(1L)).thenReturn(false);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(balanceOptional);
+        when(balanceRepository.findByCustomerId(2L)).thenReturn(balanceOptional);
+
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.transfer(transference));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+
+        assertEquals("Unprocessable entity Not enough balance", exception.getMessage());
+    }
+
+    @Test
+    void whenRefundAndTransferenceEmpty_thenThrowNotFoundException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+        when(transferenceRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> transferenceService.refund(1L));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).delete(any(Transference.class));
+    }
+
+
+    @Test
+    void whenUpdateAndPayerBalanceIsNotFound_thenThrowNotFoundException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+        Optional<Transference> transferenceOptional = Optional.of(transference);
+        when(transferenceRepository.findById(1L)).thenReturn(transferenceOptional);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> transferenceService.refund(1L));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+    }
+
+    @Test
+    void whenUpdateAndPayeeBalanceIsNotFound_thenThrowNotFoundException() {
+        Transference transference = new Transference(1L, 1L, 2L, 100.00, null);
+        Optional<Transference> transferenceOptional = Optional.of(transference);
+
+        Balance balance = new Balance(1L, 1L, 100.00);
+
+        when(transferenceRepository.findById(1L)).thenReturn(transferenceOptional);
+
+        Optional<Balance> balanceOptional = Optional.of(balance);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(balanceOptional);
+        when(balanceRepository.findByCustomerId(2L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> transferenceService.refund(1L));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+    }
+
+    @Test
+    void whenUpdateAndNotEnoughBalance_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(1L, 1L, 2L, 150.00, null);
+        Balance balance = new Balance(1L, 1L, 100.00);
+        Optional<Balance> balanceOptional = Optional.of(balance);
+
+        Optional<Transference> transferenceOptional = Optional.of(transference);
+
+        when(transferenceRepository.findById(1L)).thenReturn(transferenceOptional);
+
+        when(balanceRepository.findByCustomerId(1L)).thenReturn(balanceOptional);
+        when(balanceRepository.findByCustomerId(2L)).thenReturn(balanceOptional);
+
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.refund(1L));
+
+        verify(balanceRepository, times(0)).save(any(Balance.class));
+        verify(transferenceRepository, times(0)).save(any(Transference.class));
+
+        assertEquals("Unprocessable entity Not enough balance", exception.getMessage());
+    }
 
     @Test
     void whenFindAll_thenReturnCustomerList() {
