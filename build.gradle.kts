88dca33dbdbdf013bf260e47e8241f5fcb051904
raphacosil <raphaelcos.worker@gmail.com>
2026-04-07 11:35:50 -0300

    Updating tests and adding integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,19 +27,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-	runtimeOnly("com.mysql:mysql-connector-j")
-
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
-
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
 	testCompileOnly("org.projectlombok:lombok")
 	testAnnotationProcessor("org.projectlombok:lombok")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
@@ -51,6 +51,11 @@ dependencies {
 	testImplementation("com.h2database:h2")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:mysql:1.19.8")
+	testImplementation("org.testcontainers:junit-jupiter:1.19.8")
+
 }
 
 tasks.withType<Test> {
---
+++ src/test/java/com/example/bank_api/integration/TransferenceFlowTest.java
@@ -0,0 +1,193 @@
+package com.example.bank_api.integration;
+
+import com.example.bank_api.exception.NotFoundException;
+import com.example.bank_api.exception.UnprocessableEntityException;
+import com.example.bank_api.gateway.ApiGateway;
+import com.example.bank_api.gateway.dto.AuthorizeResponse;
+import com.example.bank_api.gateway.dto.AuthorizeResponseData;
+import com.example.bank_api.gateway.dto.SendNotificationResponse;
+import com.example.bank_api.model.Balance;
+import com.example.bank_api.model.Customer;
+import com.example.bank_api.model.Transference;
+import com.example.bank_api.repository.BalanceRepository;
+import com.example.bank_api.repository.CustomerRepository;
+import com.example.bank_api.repository.TransferenceRepository;
+import com.example.bank_api.service.TransferenceService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@Transactional
+public class TransferenceFlowTest {
+    @Autowired
+    TransferenceRepository transferenceRepository;
+    @Autowired
+    BalanceRepository balanceRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @MockitoBean
+    ApiGateway apiGateway;
+    @Autowired
+    TransferenceService transferenceService;
+
+    Customer payer;
+    Customer payee;
+
+    @BeforeEach
+    void setup() {
+        payer = customerRepository.save(
+                new Customer(null, "Payer", "12345678900", "payer@gmail.com", "password", false)
+        );
+        payee = customerRepository.save(
+                new Customer(null, "Payee", "09876543211", "payee@gmail.com", "password", false)
+        );
+
+        balanceRepository.save(new Balance(null, payer.getId(), 100.00));
+        balanceRepository.save(new Balance(null, payee.getId(), 100.00));
+    }
+
+    @AfterEach
+    void close() {
+        transferenceRepository.deleteAll();
+        balanceRepository.deleteAll();
+        customerRepository.deleteAll();
+    }
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+    }
+
+    @Test
+    void whenTransferAndNotEnoughBalance_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(null, payer.getId(), payee.getId(), 150.00, null);
+
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.transfer(transference));
+
+        Optional<Double> payerBalance = balanceRepository.findAmountByCustomerId(payer.getId());
+        Optional<Double> payeeBalance = balanceRepository.findAmountByCustomerId(payee.getId());
+
+        Assertions.assertTrue(payerBalance.isPresent());
+        Assertions.assertTrue(payeeBalance.isPresent());
+        assertEquals(Double.valueOf(100.00), payerBalance.get());
+        assertEquals(Double.valueOf(100.00), payeeBalance.get());
+
+        assertEquals("Unprocessable entity Not enough balance", exception.getMessage());
+    }
+
+    @Test
+    void whenTransferAndAuthorizeFalse_thenRefund() {
+        when(apiGateway.authorize()).thenReturn(
+                new AuthorizeResponse("fail", new AuthorizeResponseData(false))
+        );
+        when(apiGateway.sendNotification()).thenReturn(
+                new SendNotificationResponse("success", null)
+        );
+
+        Transference transference = new Transference(null, payer.getId(), payee.getId(), 100.00, null);
+
+        transferenceService.transfer(transference);
+
+        Optional<Double> payerBalance = balanceRepository.findAmountByCustomerId(payer.getId());
+        Optional<Double> payeeBalance = balanceRepository.findAmountByCustomerId(payee.getId());
+
+        Assertions.assertTrue(payerBalance.isPresent());
+        Assertions.assertTrue(payeeBalance.isPresent());
+        assertEquals(Double.valueOf(100.00), payerBalance.get());
+        assertEquals(Double.valueOf(100.00), payeeBalance.get());
+    }
+
+    @Test
+    void whenTransfer_thenReturnVoid() {
+        when(apiGateway.authorize()).thenReturn(
+                new AuthorizeResponse("success", new AuthorizeResponseData(true))
+        );
+        when(apiGateway.sendNotification()).thenReturn(
+                new SendNotificationResponse("success", null)
+        );
+
+        Transference transference = new Transference(null, payer.getId(), payee.getId(), 100.00, null);
+
+        transferenceService.transfer(transference);
+
+        Optional<Double> payerBalance = balanceRepository.findAmountByCustomerId(payer.getId());
+        Optional<Double> payeeBalance = balanceRepository.findAmountByCustomerId(payee.getId());
+
+        Assertions.assertTrue(payerBalance.isPresent());
+        Assertions.assertTrue(payeeBalance.isPresent());
+        assertEquals(Double.valueOf(0), payerBalance.get());
+        assertEquals(Double.valueOf(200.00), payeeBalance.get());
+    }
+
+    @Test
+    void whenRefundAndTransferenceEmpty_thenThrowNotFoundException() {
+        assertThrows(NotFoundException.class, () -> transferenceService.refund(2L));
+    }
+
+    @Test
+    void whenRefundAndNotEnoughBalance_thenThrowUnprocessableEntityException() {
+        Transference transference = new Transference(null, 1L, 2L, 150.00, null);
+
+        transferenceRepository.save(transference);
+
+        UnprocessableEntityException exception = assertThrows(UnprocessableEntityException.class, () -> transferenceService.refund(1L));
+
+        assertEquals("Unprocessable entity Not enough balance", exception.getMessage());
+    }
+
+    @Test
+    void whenRefund_thenReturnVoid() {
+        when(apiGateway.authorize()).thenReturn(
+                new AuthorizeResponse("success", new AuthorizeResponseData(true))
+        );
+        when(apiGateway.sendNotification()).thenReturn(
+                new SendNotificationResponse("success", null)
+        );
+
+        Transference transference = new Transference(null, payer.getId(), payee.getId(), 100.00, null);
+
+        transferenceService.transfer(transference);
+
+        Long id = transferenceRepository.findAll().getFirst().getId();
+
+        transferenceService.refund(id);
+
+        Optional<Double> payerBalance = balanceRepository.findAmountByCustomerId(payer.getId());
+        Optional<Double> payeeBalance = balanceRepository.findAmountByCustomerId(payee.getId());
+
+        Assertions.assertTrue(payerBalance.isPresent());
+        Assertions.assertTrue(payeeBalance.isPresent());
+        assertEquals(Double.valueOf(100.00), payerBalance.get());
+        assertEquals(Double.valueOf(100.00), payeeBalance.get());
+    }
+}
